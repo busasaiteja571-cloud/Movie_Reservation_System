0023fd6e90e3365adbf37f59602ad36af51b54f2
busasaiteja571-cloud <busasaiteja571@example.com>
2026-06-22 19:53:33 +0530

    chore(build): add Maven wrapper, resources, and project metadata

---
+++ Movie_Reservation_System_BackEnd/src/test/java/com/example/Movie_Reservation_System_BackEnd/MovieReservationSystemBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Movie_Reservation_System_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReservationSystemBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
